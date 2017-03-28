67a1f169df469919aebe038d24e1f3c7ad3e690a
Alyssa Broussard <alyssabroussard@legalshieldcorp.com>
2017-03-28 14:53:19 -0500

    Added more error handling

--- app/controllers/api/meetings_controller.rb
+++ app/controllers/api/meetings_controller.rb
@@ -16,20 +16,29 @@ class API::MeetingsController < ApplicationController
       render json: { error: "You have not entered the parameters correctly. Please try again."}, status: :unprocessable_entity
     else
       @reason = params_array[0]
+
       start_time_arr = params_array[1].scan(/\d+|\w+/)
-      if start_time_arr[5].start_with?('p', 'P')
-        start_time_hour = start_time_arr[3].to_i + 12
+      if start_time_arr.length != 6
+        render json: { error: "You have not entered the start time parameter correctly. Please try again."}, status: :unprocessable_entity
       else
-        start_time_hour = start_time_arr[3]
+        if start_time_arr[5].start_with?('p', 'P')
+          start_time_hour = start_time_arr[3].to_i + 12
+        else
+          start_time_hour = start_time_arr[3]
+        end
+        @start_time = DateTime.new(start_time_arr[2].to_i, start_time_arr[0].to_i, start_time_arr[1].to_i, start_time_hour.to_i, start_time_arr[4].to_i)
       end
-      @start_time = DateTime.new(start_time_arr[2].to_i, start_time_arr[0].to_i, start_time_arr[1].to_i, start_time_hour.to_i, start_time_arr[4].to_i)
       end_time_arr = params_array[2].scan(/\d+|\w+/)
-      if end_time_arr[5].start_with?('p', 'P')
-        end_time_hour = end_time_arr[3].to_i + 12
+      if start_time_arr.length != 6
+        render json: { error: "You have not entered the start time parameter correctly. Please try again."}, status: :unprocessable_entity
       else
-        end_time_hour = end_time_arr[3]
+        if end_time_arr[5].start_with?('p', 'P')
+          end_time_hour = end_time_arr[3].to_i + 12
+        else
+          end_time_hour = end_time_arr[3]
+        end
+        @end_time = DateTime.new(end_time_arr[2].to_i, end_time_arr[0].to_i, end_time_arr[1].to_i, end_time_hour.to_i, end_time_arr[4].to_i)
       end
-      @end_time = DateTime.new(end_time_arr[2].to_i, end_time_arr[0].to_i, end_time_arr[1].to_i, end_time_hour.to_i, end_time_arr[4].to_i)
       @user = User.where("slackhandle = ?", params[:user_name].to_s).take!
       @meeting = @user.meetings.build(name: @user.full_name, reason: @reason, start_time: @start_time, end_time: @end_time)
 
