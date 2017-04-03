a7a35309021b7d7bd2092485d3132558ebcdc7b2
Alyssa Broussard <alyssabroussard@legalshieldcorp.com>
2017-04-03 10:28:52 -0500

    Changes to slack response task

--- lib/slack_response.rb
+++ lib/slack_response.rb
@@ -2,23 +2,20 @@ class SlackResponse
   attr_reader :title, :text, :title_link, :image_url, :thumb_url, :footer, :footer_icon, :color
 
   def initialize(params = {})
-    @title_link  = 'https://cryptic-woodland-68868.herokuapp.com/instructions'
-    @image_url   = 'http://my-website.com/path/to/image.jpg'
-    @thumb_url   = 'http://example.com/path/to/thumb.png'
-    @footer      = 'Slack API'
-    @footer_icon = 'https://platform.slack-edge.com/img/default_application_icon.png'
-    @title       = 'OOO Bot'
-    @text        = 'This is a post'
-    @pretext     = 'OOO Bot'
+    @failed = params[:failed] || false
+    @title_link  = params[:title_link] || 'https://cryptic-woodland-68868.herokuapp.com/instructions'
+    @image_url   = params[:image_url] || 'http://my-website.com/path/to/image.jpg'
+    @thumb_url   = params[:thumb_url] || 'http://example.com/path/to/thumb.png'
+    @footer      = params[:footer] || 'Slack API'
+    @footer_icon = params[:footer_icon] || 'https://platform.slack-edge.com/img/default_application_icon.png'
+    @title       = params[:title] || 'OOO Bot'
+    @text        = params[:text] || 'This is a post'
+    @pretext     = params[:pretext] || 'OOO Bot'
 
     if params[:failed]
       @color = 'danger'
     else
       @color = 'good'
-    end
-
-    params.each do |key, value|
-      self.instance_variable_set(:"@#{key}", value)
     end
   end
 
--- spec/controllers/api/meetings_controller_spec.rb
+++ spec/controllers/api/meetings_controller_spec.rb
@@ -53,7 +53,8 @@ RSpec.describe API::MeetingsController, type: :controller do
 
       body = JSON.parse(response.body)
       expect(body['attachments'][0]['title']).to eq("Out of Office Calendar Help")
-      expect(body['attachments'][0]['text']).to eq("Just enter /ooo [reason, start time, end time] and your event will automatically be added to the Out of Office Calendar! Make sure the parameters are separated by commas and the times are in the format: MM-DD-YYYY HH:MM am/pm. For example: /ooo WFH, 03-27-2017 8:00 am, 03-27-2017 5:00 pm")
+      expect(body['attachments'][0]['text']).to eq("Just enter /ooo [reason, start time, end time] and your event will automatically be added to the Out of Office Calendar! Make sure the parameters are separated by commas and the times are in the format: MM-DD-YYYY HH:MM am/pm. For example: /ooo WFH, 03-27-2017 8:00 am, 03-27-2017 5:00 pm
+      You can also see if an employee is in or out of the office today by entering /ooo @slack_name. For example: /ooo @alyssa")
     end
   end
 
